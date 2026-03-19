9008687a92b5d0f3c08e105f71bcfba68b701bcc
SushmithaKatika <katikasushmitha7228@gmail.com>
2026-03-19 14:02:10 +0530

    Cleaned repository structure

--- .vscode/Manager.java
+++
@@ -1,9 +0,0 @@
-public class Manager extends Employee{
-    public Manager(String name){
-        super(name);
-    }
-    @Override
-    public int calculateSalary(){
-        return baseSalary + 100;
-    }
-}
